e90658dc2fca71173f2359005219ddcebc087f35
fatimalvcruz <fatimalvcruz@gmail.com>
2022-09-05 14:13:27 +0200

    model + interface included

---
+++ src/app/models/shop.model.ts
@@ -0,0 +1,29 @@
+export class ShopModel {
+
+    shopItems: object;
+ 
+    constructor() {
+     this.shopItems = [
+       {
+       title: 'Curso Wordpress',
+       desc: 'Aprende Wordpress desde 0 con David Piqué',
+       picture: 'wordpress.jpg',
+       price: 164
+       },
+       {
+       title: 'Curso Frontend',
+       desc: 'Aprende desarrollo front end desde 0 con David Piqué',
+       picture: 'frontend.jpg',
+       price: 220
+       },
+       {
+       title: 'Curso Fullstack',
+       desc: 'Aprende desarrollo frontend y backend desde 0 con David Piqué',
+       picture: 'fullstack.jpg',
+       price: 420
+       }
+     ];
+    }
+
+
+}
